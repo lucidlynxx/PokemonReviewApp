1930874993c2be4ebf0c43e5e88f8610281e96ea
lucidlynxx <dzakysyahrizal04@gmail.com>
2024-09-22 09:28:45 +0700

    menambahkan file gitignore

--- PokemonReviewApp.Tests/obj/Debug/net8.0/PokemonReviewApp.Tests.AssemblyInfo.cs
+++ PokemonReviewApp.Tests/obj/Debug/net8.0/PokemonReviewApp.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonReviewApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9dcb0e3314285376352a1634e568db3ddb194e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonReviewApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonReviewApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PokemonReviewApp/obj/Debug/net8.0/PokemonReviewApp.AssemblyInfo.cs
+++ PokemonReviewApp/obj/Debug/net8.0/PokemonReviewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9dcb0e3314285376352a1634e568db3ddb194e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
